61c4c9938ba0de7dd21e302676e6415ca3fde181
Jiwoong Song <helldongdae@gmail.com>
2016-11-11 21:45:19 +0900

    2016 11 11
    HW #1

--- README.md
+++ README.md
@@ -4,16 +4,25 @@ MARKDOWN 2 HTML CONVERTER v.1.0
 
 How to use
 
-1. import cmdReader class
+If you need help, type in HELP
+	Ex) java mdToHtml HELP
 
-2. import java.io.* class
+1. Execute mdToHtml.java file
 
-3. receive command line arguments in form of : 
+2. Type in arguments in form of <filename option>
+	Ex) java mdToHtml file.md fancy
+   if you want to give multiple md files as input, then 
+    Ex) java mdToHtml file1.md file2.md fancy
 
-	FILENAME.md OPTION or FILENAME.md or HELP
+3. Program will consider if given inputs are valid.
 
-4. make an instance of cmdReader and give arguments as its parameter.
- 
+4. If your input is invalid, the program will stop and tell you the reason why your input is invalid.
+
+5. If your input is valid, the program will return result as:
+	Received valid arguments
+	Option : plain
+	# of inputs: 1
+
 ***VALID OPTIONS***
 
 A. plain
@@ -21,9 +30,14 @@ A. plain
 B. fancy
 
 C. slide
+
 ## All options are CASE SENSITIVE
 5. if you type in FILENAME.md and OPTION, program will read the specified file name from the classpath. If the filename is invalid or the file does not exist or the option is invalid, the program will print error message and terminate.
+
 6. if you type in only FILENAME.md, program will read the specified file name from the classpath. If the filename is invalid or the file does not exist, the program will print error message and terminate.
+
 7. if you type in only HELP, program will print out help message and terminate.
+
 8. if there exist an error, only one error will be told.
+
 9. if there is no error, then the the content of the file specified by FILENAME will be stored in <file> inputfilestream in the instance of cmdReader.
--- mdToHtml.java
+++ mdToHtml.java
@@ -1,22 +1,58 @@
 import java.io.*;
 
-public class mdToHtml{
+public class mdToHtml1{
 public static void main(String args[]) {
     cmdReader reader = new cmdReader(args);
-    FileInputStream f = reader.file;
+    System.out.println("Received valid arguments");
+    System.out.println("Option: "+ reader.option);
+    System.out.print("# of inputs: ");
+    System.out.println(reader.numOfMd);
   }
 }
 
-class cmdReader extends mdToHtml{
-    cmdReader(String args[]){
-        if(!checkIfValid(args))
-            System.exit(101);
-        file = readFile(args[0]);
-        if(file == null)
-            System.exit(102);
-    }
+class cmdReader extends mdToHtml1{
+    public FileInputStream file[] = null;
+    public String filenames[] = null;
+    public String option;
+    public int numOfMd;
 
-    public FileInputStream file;
+    cmdReader(String args[]){
+        if(args.length == 0){
+            System.out.println("Invalid arguments!");
+            System.exit(101);
+        }
+        if(args.length == 1 && args[0].equals("HELP"))
+            help();
+        numOfMd = howManyMds(args);
+        if(numOfMd == 0){
+            System.out.println("No md file is given!");
+            System.exit(101);
+        }
+        file = new FileInputStream[numOfMd];
+        filenames = new String[numOfMd];
+        int i;
+        for(i = 0; i < numOfMd; i++){
+            if(checkIfRedundant(args[i], i)){
+                System.out.println("Redundant file name!");
+                System.exit(101);
+            }
+            file[i] = readFile(args[i]);
+            filenames[i] = args[i];
+        }
+        if(i == args.length-1){
+            if(!checkIfValidOption(args[args.length-1])){
+                System.out.println("Invalid Option!");
+                System.exit(101);
+            }
+            option = args[i];
+        }
+        else if(i == args.length)
+            option = "plain";
+        else{
+            System.out.println("Invalid Option!");
+            System.exit(101);
+        }
+    }
 
     void help(){
         System.out.println("-----------HELP-------------");
@@ -27,48 +63,48 @@ class cmdReader extends mdToHtml{
         System.out.println("argument 1 = File name");
         System.out.println("argument 2 = Option   ");
         System.out.println("________________________________");
+        System.exit(101);
     }   
 
-    boolean checkIfValid(String arguments[]){
-    // Check if given file is a valid md file
+    boolean checkIfRedundant(String s, int n){
+        for(int i = 0;i < n;i++){
+            if(filenames[i].equals(s))
+                return true;
+        }
+        return false;
+    }
+
+    int howManyMds(String arguments[]){
+        int ret = 0;
+        for(int i = 0;i<arguments.length;i++){
+            if(isMd(arguments[i]))
+                ret++;  
+        }
+        return ret;
+    }
+
+    boolean checkIfValidOption(String option){
+       if(option.equals("plain") || option.equals("fancy") || option.equals("slide"))
+        return true;
+       else
+        return false;
+    }
+
+    boolean isMd(String name){
         int locationOfDot = 0;
         String checkValidMD = null;
 
-        if(arguments.length > 2 || arguments.length == 0){
-            System.out.println("ERROR : Invalid command line arguments!");
-            return false;
-        }
-
-        if(arguments[0].equals("HELP")){
-            help();
-            return false;
-        }
-
-        locationOfDot = arguments[0].indexOf('.');
+        locationOfDot = name.indexOf('.');
         
-        if(locationOfDot == -1){
-            System.out.println("ERROR : Invalid fle name!");
+        if(locationOfDot == -1)
             return false;
-        }
         
-        checkValidMD = arguments[0].substring(locationOfDot);    
+        checkValidMD = name.substring(locationOfDot);    
         
-        if(!checkValidMD.equals(".md")){
-            System.out.println("ERROR: Invalid fle name!");
+        if(!checkValidMD.equals(".md"))
             return false;
-        }
 
-        if(arguments.length > 1){
-            if(arguments[1].equals("plain") || arguments[1].equals("fancy") || arguments[1].equals("slide"))
-                return true;
-            else{
-                System.out.println("ERROR: Invalid Option!");
-                return false;
-            }
-        }
-        else if(arguments.length == 1)
-           return true;
-       return true;
+        return true;
     }
 
     FileInputStream readFile(String fileName){
@@ -78,6 +114,7 @@ class cmdReader extends mdToHtml{
         }
         catch(Exception e){
             System.out.println("ERROR: File does not exist!");
+            System.exit(101);
         }
         return f;
     }
